e21bdad084fe9e76667a3f50badb981656dea086
fireab <freab4@gmail.com>
2025-08-04 09:03:22 +0300

    fix: add static ip for the desktop app

--- lib/helpers/EmployeeHelper.dart
+++ lib/helpers/EmployeeHelper.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:nifas_silk/constants/Employees.dart';
 
 class EmployeeService {
-  static const String apiUrl = 'http://localhost:2000/employee/';
+  static const String apiUrl = 'http://10.33.79.21:2000/employee/';
 
   static Future<List<Employee>> fetchEmployees() async {
     final response = await http.get(Uri.parse(apiUrl));
